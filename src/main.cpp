b277edfe31a03c55178451d4213996daa7caf695
DHipo <104331457+DHipo@users.noreply.github.com>
2024-11-22 10:52:50 -0300

    src folder

--- src/main.cpp
+++ src/main.cpp
@@ -61,7 +61,7 @@ void UpdateDrawFrame(void)
 
         ClearBackground(RAYWHITE);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Congrats! You created your first windowas!", 190, 200, 20, LIGHTGRAY);
 
     EndDrawing();
     //----------------------------------------------------------------------------------
